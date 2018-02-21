34652c1cc48e238921ac57583fad2425e4a4b344
Antonio Vivace <puma95.95@gmail.com>
2018-02-21 15:49:21 +0100

    template with express/ejs

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ var express	= require('express'),
 	morgan	= require('morgan');
 
 app.use(morgan('dev'));
+
+app.set('view engine', 'ejs');
+app.set('views', './html')
+
 app.use('/', express.static('html'))
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.get('/editor', function (req, res) {
+  res.render('editor')
+})
+
+app.get('/settings', function (req, res) {
+  res.render('settings')
+})
+
 
 app.listen(port);
