0208b8c7068d72c01db99172f1d9625e889322e8
Antonio Vivace <puma95.95@gmail.com>
2018-02-26 20:23:04 +0100

    General settings tab layout, autosave entry

--- html/workspace_factory/js/blocklyfactory.js
+++
@@ -1,9 +0,0 @@
-var basicToolbox = Toolbox.FULL;
-
-var toolbox = '<xml></xml>';
-var workspaceBlocks = '<xml></xml>';
-
-var factoryController = new FactoryController(toolbox, workspaceBlocks);
-var preview = new Preview(factoryController);
-factoryController.setPreview(preview);
-factoryController.injectWorkspaces();
